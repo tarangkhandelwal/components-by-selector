7791db29ef0f1f15200a0d4fd65d4a4a2cd95e34
Tarang Khandelwal <khandelwalt@vmware.com>
2020-12-17 21:27:32 +0530

    Updating example to include dynamic component with inputs

--- src/app/child1/dynamic1/dynamic1.component.ts
+++ src/app/child1/dynamic1/dynamic1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dynamic1',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dynamic1.component.css']
 })
 export class Dynamic1Component implements OnInit {
+
+  @Input() inputValue = "default-value";
 
   constructor() { }
 
